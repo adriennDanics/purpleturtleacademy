2fb7a7a278ff97c66c863260e067a3df0ac08472
Adrienn <adrienn.d91@gmail.com>
2018-05-29 12:54:05 +0200

    Course added

--- src/main/java/com/codecool/PTA/Academy.java
+++ src/main/java/com/codecool/PTA/Academy.java
@@ -1,4 +1,7 @@
 package com.codecool.PTA;
 
 public class Academy {
+    public static void main(String[] args) {
+
+    }
 }
---
+++ src/main/java/com/codecool/PTA/course/Course.java
@@ -0,0 +1,4 @@
+package com.codecool.PTA.course;
+
+public class Course {
+}
