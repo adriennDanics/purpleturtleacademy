1b250052799337c038285dbb8cd68689a392de35
tamasVarjas <tamas.varjas00@gmail.com>
2018-05-29 14:37:06 +0200

    Create User and subclasses, together with annotations

--- src/main/java/com/codecool/PTA/user/Mentor.java
+++ src/main/java/com/codecool/PTA/user/Mentor.java
@@ -1,4 +1,15 @@
 package com.codecool.PTA.user;
 
+import javax.persistence.*;
+
+@Entity
 public class Mentor extends User {
+
+    public Mentor() {
+        super();
+    }
+
+    public Mentor(String username, String password) {
+        super(username, password);
+    }
 }
--- src/main/java/com/codecool/PTA/user/Student.java
+++ src/main/java/com/codecool/PTA/user/Student.java
@@ -1,4 +1,22 @@
 package com.codecool.PTA.user;
 
-public class Student extends User{
+import javax.persistence.*;
+
+@Entity
+public class Student extends User {
+
+    private long xp;
+
+    @Enumerated(EnumType.STRING)
+    private Level level;
+
+    public Student() {
+        super();
+    }
+
+    public Student(String username, String password) {
+        super(username, password);
+        this.xp = 0;
+        this.level = Level.BEGINNER;
+    }
 }
