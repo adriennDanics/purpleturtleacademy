4e692154fdc234e9cfa0d4771e3f23266a632080
Tamas Horvath <horvathtamas22@gmail.com>
2018-05-29 13:40:17 +0200

    login template and controller

--- src/main/java/com/codecool/PTA/controller/LoginController.java
+++ src/main/java/com/codecool/PTA/controller/LoginController.java
@@ -1,14 +1,20 @@
 package com.codecool.PTA.controller;
 
 import com.codecool.PTA.config.TemplateEngineUtil;
+import com.codecool.PTA.user.Student;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
+import javax.persistence.*;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.List;
+
+import java.text.ParseException;
 
 
 @WebServlet(urlPatterns = {"/"})
@@ -18,10 +24,34 @@ public class LoginController extends AbstractController {
         protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
             WebContext context = new WebContext(req, resp, req.getServletContext());
-            context.setVariable("recipient", "World");
 
             TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
             engine.process("index.html", context, resp.getWriter());
 
         }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("ptadb");
+        EntityManager em = emf.createEntityManager();
+
+        Query getAllStudents = em.createQuery("SELECT stud FROM Student stud");
+
+        List<Student> studentList = getAllStudents.getResultList();
+        for (Student student : studentList) {
+            if(username.equals(student.getUsername())) {
+                if(password.equals(student.getHashedPassword())) {//Hash.isPasswordCorrect(password, customer.getHashedPassword())) {
+                    session.setAttribute("student", student);
+                    resp.sendRedirect("/");
+                }
+            } else {
+                resp.sendRedirect("/");
+            }
+        }
+    }
 }
