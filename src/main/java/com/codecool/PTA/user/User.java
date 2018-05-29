786c3abf7b87893eb924318eb591ced9ab3f74b4
tamasVarjas <tamas.varjas00@gmail.com>
2018-05-29 14:55:20 +0200

    Add getters and setters for username and password

--- src/main/java/com/codecool/PTA/user/User.java
+++ src/main/java/com/codecool/PTA/user/User.java
@@ -61,7 +61,23 @@ public abstract class User {
         this.email = email;
     }
 
-//    public static void main(String[] args) {
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    //    public static void main(String[] args) {
 //
 //        EntityManagerFactory emf = Persistence.createEntityManagerFactory("ptaPU");
 //        EntityManager em = emf.createEntityManager();
