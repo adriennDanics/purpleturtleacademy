bcdffe189e20c932bfb0814e5c4ae4cf5d5b7c03
Adrienn <adrienn.d91@gmail.com>
2018-05-30 16:53:40 +0200

    Login check and additional design

--- src/main/java/com/codecool/PTA/controller/CoursesController.java
+++ src/main/java/com/codecool/PTA/controller/CoursesController.java
@@ -16,11 +16,13 @@ public class CoursesController extends AbstractController {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if(checkLogin(req)) {
+            WebContext context = new WebContext(req, resp, req.getServletContext());
 
-        WebContext context = new WebContext(req, resp, req.getServletContext());
-
-        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
-        engine.process("courses/courses.html", context, resp.getWriter());
-
+            TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
+            engine.process("courses/courses.html", context, resp.getWriter());
+        } else {
+            resp.sendRedirect("/login");
+        }
     }
 }
