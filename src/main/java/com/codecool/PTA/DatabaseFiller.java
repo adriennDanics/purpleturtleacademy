996894dcc260e5ee0ee8abb019cc7c971526d8cc
Bende Hajdu <outdoorsport85@gmail.com>
2018-05-31 14:25:05 +0200

    AssignmentController

--- src/main/java/com/codecool/PTA/DatabaseFiller.java
+++ src/main/java/com/codecool/PTA/DatabaseFiller.java
@@ -2,7 +2,7 @@ package com.codecool.PTA;
 
 import com.codecool.PTA.config.AssignmentConfig;
 
-public class Academy {
+public class DatabaseFiller {
     public static void main(String[] args) {
         AssignmentConfig assignmentConfig = new AssignmentConfig();
         assignmentConfig.fillDB();
