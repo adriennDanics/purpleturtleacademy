2ba92b0e8b4e5195e40e9f23e513c509c67f90e2
Tamas Horvath <horvathtamas22@gmail.com>
2018-06-04 16:48:49 +0200

    correct answers are transformed to a list for checking

--- src/main/webapp/static/js/fill.js
+++ src/main/webapp/static/js/fill.js
@@ -1,25 +1,47 @@
 dom = {
-    _cucc: "",
     _id: "",
+    _correctAnswersObject: "",
+    _correctAnswersList: [],
+    _userAnswers: [],
 
     init: function () {
-        dom.getAnswers();
+        dom.getCorrectAnswers();
+        dom.getUserAnswers();
     },
-
-    // checkAnswers: function () {
-    //     let answerInputs = document.getElementsByClassName("input");
-    //     let submitButton = document.getElementById("submit");
-    //     submitButton.addEventListener('click', function () {
-    //
-    //     })
-    // },
     
-    getAnswers: function () {
+    getCorrectAnswers: function() {
         let questionId = document.getElementById("fillInId").innerText;
         dom._id = questionId;
-        $.getJSON("/fill_in_answers?id=" + questionId, function(response) {
-            dom._cucc = response;
+        // $.getJSON("/fill_in_answers?id=" + questionId, function(response) {
+        //     dom._correctAnswersObject = response;
+        // });
+
+        fetch("/fill_in_answers?id=" + questionId)
+            .then(response => response.json())
+            .then(function(result) {
+                dom._correctAnswersObject = result;
+                dom.makeCorrectAnswerList();
+            });
+    },
+
+    makeCorrectAnswerList: function() {
+        for(let key in dom._correctAnswersObject) {
+            dom._correctAnswersList.push(dom._correctAnswersObject[key])
+        }
+    },
+
+    getUserAnswers: function() {
+        let submitButton = document.getElementById("submit");
+        submitButton.addEventListener('click', function () {
+            let answerInputs = document.getElementsByClassName("answer");
+            for(let answer of answerInputs) {
+                dom._userAnswers.push(answer.value);
+            }
         })
+    },
+
+    checkAnswers: function() {
+
     }
 
 };
