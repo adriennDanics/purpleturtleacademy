1be3171a343bcd51667a46c71c92e617fbda73e9
Tamas Horvath <horvathtamas22@gmail.com>
2018-06-04 13:15:55 +0200

    JS file added

---
+++ src/main/webapp/static/js/fill.js
@@ -0,0 +1,9 @@
+dom = {
+    checkAnswers: function () {
+        let answerInputs = document.getElementsByClassName("input");
+        let submitButton = document.getElementById("submit");
+        submitButton.addEventListener('click', function () {
+            
+        })
+    }
+};
