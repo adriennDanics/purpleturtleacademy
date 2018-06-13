647761982037b729a4b75bdd3b0f234d6aad64ff
Tamás Varjas <tamas.varjas00@gmail.com>
2018-06-13 13:12:34 +0200

    create test DB for persistence testing purposes

--- tests/com/codecool/PTA/persistence/PersistenceImplementationTest.java
+++ tests/com/codecool/PTA/persistence/PersistenceImplementationTest.java
@@ -1,10 +1,23 @@
 package com.codecool.PTA.persistence;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class PersistenceImplementationTest {
+
+    private EntityManager em;
+
+    @BeforeAll
+    void setup() {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("pta-testPU");
+        this.em = emf.createEntityManager();
+    }
 
     @Test
     void persist() {
