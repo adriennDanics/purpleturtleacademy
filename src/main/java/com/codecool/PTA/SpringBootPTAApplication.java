29d43f3f550ece764142e0ea70c141405ff75c8a
Tamás Varjas <tamas.varjas00@gmail.com>
2018-06-25 13:23:49 +0200

    create spring boot application

---
+++ src/main/java/com/codecool/PTA/SpringBootPTAApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.PTA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPTAApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootPTAApplication.class, args);
+    }
+
+}
