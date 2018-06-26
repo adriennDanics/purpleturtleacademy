98185d169daaadc8bd324ef302ff6cbd57727407
Ben85 <outdoorsport85@gmail.com>
2018-06-26 14:56:25 +0200

    SecurityConfig

---
+++ src/main/java/com/codecool/PTA/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.codecool.PTA.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+@EnableGlobalMethodSecurity(securedEnabled = true)
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/")
+                .permitAll()
+                .antMatchers("assignments/").hasRole("USER")
+                .and()
+                .formLogin()
+                .permitAll();
+    }
+}
