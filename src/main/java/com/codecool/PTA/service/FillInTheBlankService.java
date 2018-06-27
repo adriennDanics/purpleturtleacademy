d89a7087a86f39768b92cdb90a674a9e569a412a
Tamas Horvath <horvathtamas22@gmail.com>
2018-06-27 10:13:56 +0200

    display fill assignments

---
+++ src/main/java/com/codecool/PTA/service/FillInTheBlankService.java
@@ -0,0 +1,16 @@
+package com.codecool.PTA.service;
+
+import com.codecool.PTA.model.quest.FillInTheBlank;
+import com.codecool.PTA.repository.FillInTheBlankRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FillInTheBlankService {
+
+    @Autowired
+    private FillInTheBlankRepository fillInTheBlankRepository;
+
+    public FillInTheBlank findById(Long id) {return fillInTheBlankRepository.findOne(id)}
+
+}
