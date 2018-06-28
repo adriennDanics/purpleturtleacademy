6f640b822729526d5f7b0f60af33e38775357f36
Tamás Varjas <tamas.varjas00@gmail.com>
2018-06-28 15:56:57 +0200

    DB filling works!!!

--- src/main/java/com/codecool/PTA/repository/RoleRepository.java
+++ src/main/java/com/codecool/PTA/repository/RoleRepository.java
@@ -1,9 +1,11 @@
 package com.codecool.PTA.repository;
 
 import com.codecool.PTA.model.role.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface RoleRepository extends CrudRepository {
+public interface RoleRepository extends JpaRepository<Role, Long> {
 
     public Role findByRole(String role);
+
 }
--- src/main/java/com/codecool/PTA/service/InitializerBean.java
+++ src/main/java/com/codecool/PTA/service/InitializerBean.java
@@ -3,30 +3,38 @@ package com.codecool.PTA.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+
 @Component
 public class InitializerBean {
 
     @Autowired
     private ExampleEntityCreator exampleEntityCreator;
+    @Autowired
+    private StudentService studentService;
+    @Autowired
+    private CourseService courseService;
+    @Autowired
+    private FillInAnswerService fillInAnswerService;
+    @Autowired
+    private FillInTheBlankService fillInTheBlankService;
+    @Autowired
+    private QuizQuestionService quizQuestionService;
+    @Autowired
+    private KataService kataService;
+    @Autowired
+    private PAService paService;
 
     public InitializerBean() {
     }
 
-    public InitializerBean(
-            StudentService studentService,
-            CourseService courseService,
-            FillInAnswerService fillInAnswerService,
-            FillInTheBlankService fillInTheBlankService,
-            QuizQuestionService quizQuestionService,
-            KataService kataService,
-            PAService paService
-    ) {
-        this();
+    @PostConstruct
+    private void fillDB() {
         exampleEntityCreator.fillData();
-        exampleEntityCreator.studentList.forEach(student -> studentService.save(student));
         exampleEntityCreator.courseList.forEach(course -> courseService.save(course));
-        exampleEntityCreator.fillInAnswerList.forEach(fillInAnswer -> fillInAnswerService.save(fillInAnswer));
+        exampleEntityCreator.studentList.forEach(student -> studentService.save(student));
         exampleEntityCreator.fillInTheBlankList.forEach(fillInTheBlank -> fillInTheBlankService.save(fillInTheBlank));
+        exampleEntityCreator.fillInAnswerList.forEach(fillInAnswer -> fillInAnswerService.save(fillInAnswer));
         exampleEntityCreator.questionList.forEach(question -> quizQuestionService.save(question));
         exampleEntityCreator.kataList.forEach(kata -> kataService.save(kata));
         exampleEntityCreator.paList.forEach(pa -> paService.save(pa));
